e8b6446fe445ef429007cc84441dccb7a698a2cc
Didar Yermekuly <yermekulyd@gmail.com>
2024-01-31 00:13:19 +0600

    [add] adding layout to the app

---
+++ app/layout.jsx
@@ -0,0 +1,22 @@
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "PromptWiki",
+  description: "Discover & Share AI Prompts",
+};
+
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
