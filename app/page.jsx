95ada6990b13fb14270d53db5c8716fdfed8447b
Didar Yermekuly <yermekulyd@gmail.com>
2024-01-31 00:25:03 +0600

    [add] modifying home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">
+        Discover & Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center">AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        PromptWiki is an open-source AI prompting tool for modern world to
+        discover, create and share createive prompts
+      </p>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
